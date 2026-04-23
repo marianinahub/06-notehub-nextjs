5b840a764213520e3fd362c54d8344abeabe5c73
Nina Aksenich <maruaninashved1488@gmail.com>
2026-04-23 17:43:31 +0300

    min fix

---
+++ notehub/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import css from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={css.footer}>
+      <div className={css.content}>
+        <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
+        <div className={css.wrap}>
+          <p>Developer: Yelyzaveta</p>
+          <p>
+            Contact us:
+            <a href="mailto:vecheli0306@gmail.com">vecheli0306@gmail.com</a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
